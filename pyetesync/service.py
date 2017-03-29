fbb36262fd61990a11cd065c9aeb0493f36436e5
Tom Hacohen <tom@stosb.com>
2017-03-29 23:56:26 +0100

    Add service.py to handle interaction with the service.

---
+++ pyetesync/service.py
@@ -0,0 +1,127 @@
+from urllib.request import build_opener
+import urllib.parse
+import json
+import base64
+import binascii
+from furl import furl
+
+from pyetesync.crypto import CryptoManager, HMAC_SIZE
+
+API_PATH = ('api', 'v1')
+
+
+class Authenticator:
+    def __init__(self, remote):
+        self.remote = furl(remote)
+        self.remote.path.segments.extend(('api-token-auth', ''))
+        self.remote.path.normalize()
+
+    def get_auth_token(self, username, password):
+        details = urllib.parse.urlencode({'username': username, 'password': password}).encode('utf-8')
+        request = urllib.request.Request(self.remote.url, details)
+        response = urllib.request.urlopen(request)
+        data = json.loads(response.read().decode())
+        return data['token']
+
+
+class Base:
+    def __init__(self, cryptoManager, content, uid):
+        self.cryptoManager = cryptoManager
+        self.uid = uid
+        self.version = cryptoManager.version
+        self.content = content
+
+    def getContent(self):
+        return self.cryptoManager.decrypt(self.content)
+
+
+class Journal(Base):
+    def __init__(self, cryptoManager, content, uid):
+        super().__init__(cryptoManager, content, uid)
+        self.hmac = content[:HMAC_SIZE]
+        self.content = content[HMAC_SIZE:]
+
+    def calcHmac(self):
+        return self.cryptoManager.hmac(self.uid.encode('utf-8') + self.content)
+
+    def verify(self):
+        if self.calcHmac() != self.hmac:
+            raise Exception('HMAC MISMATCH')
+
+
+class Entry(Base):
+    def calcHmac(self, prev):
+        prevUid = b''
+        if prev is not None:
+            prevUid = prev.uid.encode('utf-8')
+
+        return self.cryptoManager.hmac(prevUid + self.content)
+
+    def verify(self, prev):
+        if self.calcHmac(prev) != binascii.unhexlify(self.uid):
+            raise Exception('HMAC MISMATCH')
+
+
+class BaseManager:
+    def __init__(self, remote, authToken):
+        self.remote = furl(remote)
+        self.remote.path.segments.extend(API_PATH + ('journals', ''))
+        self.remote.path.normalize()
+        self.opener = build_opener()
+        self.opener.addheaders = [('Authorization', 'Token ' + authToken)]
+
+    def list(self, password):
+        response = self.opener.open(self.remote.url)
+        data = json.loads(response.read().decode())
+        for j in data:
+            uid = j['uid']
+            version = j['version']
+            content = base64.b64decode(j['content'])
+            cryptoManager = CryptoManager(version, password, uid.encode('utf-8'))
+            journal = Journal(cryptoManager=cryptoManager, content=content, uid=uid)
+            yield journal
+
+
+class JournalManager:
+    def __init__(self, remote, authToken):
+        self.remote = furl(remote)
+        self.remote.path.segments.extend(API_PATH + ('journals', ''))
+        self.remote.path.normalize()
+        self.opener = build_opener()
+        self.opener.addheaders = [('Authorization', 'Token ' + authToken)]
+
+    def list(self, password):
+        response = self.opener.open(self.remote.url)
+        data = json.loads(response.read().decode())
+        for j in data:
+            uid = j['uid']
+            version = j['version']
+            content = base64.b64decode(j['content'])
+            cryptoManager = CryptoManager(version, password, uid.encode('utf-8'))
+            journal = Journal(cryptoManager=cryptoManager, content=content, uid=uid)
+            yield journal
+
+
+class EntryManager:
+    def __init__(self, remote, authToken, journalId):
+        self.remote = furl(remote)
+        self.remote.path.segments.extend(API_PATH + ('journal', journalId, ''))
+        self.remote.path.normalize()
+        self.opener = build_opener()
+        self.opener.addheaders = [('Authorization', 'Token ' + authToken)]
+
+    def list(self, cryptoManager, last=None):
+        remote = self.remote.copy()
+        prev = None
+        if last is not None:
+            prev = Entry(cryptoManager, b'', last)
+            remote.args['last'] = last
+        response = self.opener.open(remote.url)
+        data = json.loads(response.read().decode())
+        for j in data:
+            uid = j['uid']
+            content = base64.b64decode(j['content'])
+            entry = Entry(cryptoManager=cryptoManager, content=content, uid=uid)
+            entry.verify(prev)
+            prev = entry
+            yield entry
