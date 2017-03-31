0b76dc8530ddd533e82c65564f14f670bb039bc3
Tom Hacohen <tom@stosb.com>
2017-03-31 17:55:55 +0100

    db: Move default db location to ~/.pyetesync/data.db

--- pyetesync/db.py
+++ pyetesync/db.py
@@ -1,7 +1,14 @@
+import os
+
 import peewee as pw
 from playhouse.sqlite_ext import SqliteExtDatabase
 
-db = SqliteExtDatabase('etesync.db')
+directory = os.path.join(os.path.expanduser('~'), '.pyetesync')
+
+if not os.path.exists(directory):
+    os.makedirs(directory)
+
+db = SqliteExtDatabase(os.path.join(directory, 'data.db'))
 db.connect()
 
 
