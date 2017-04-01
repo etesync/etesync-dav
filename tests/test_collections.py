460a730cfe630df750fe9fcddf17d71eafb67cff
Tom Hacohen <tom@stosb.com>
2017-04-01 16:10:11 +0100

    Add basic tests.

---
+++ tests/test_collections.py
@@ -0,0 +1,105 @@
+import pytest
+import binascii
+
+import pyetesync as api
+from pyetesync import exceptions
+from pyetesync.crypto import hmac256
+
+
+USER_EMAIL = 'test@localhost'
+TEST_REMOTE = 'http://localhost:8000'
+TEST_DB = ':memory:'
+
+
+# Gets fake (consistent between tests) random numbers
+def get_random_uid(context):
+    context._rand = getattr(context, '_rand', 0)
+    context._rand = context._rand + 1
+    return binascii.hexlify(hmac256(b'', str(context._rand).encode())).decode()
+
+
+@pytest.fixture(scope="module")
+def etesync():
+    return api.EteSync(USER_EMAIL, '', remote=TEST_REMOTE, db_path=TEST_DB)
+
+
+class TestCollection:
+    @pytest.fixture(autouse=True)
+    def transact(self, request, etesync):
+        etesync._init_db(TEST_DB)
+        yield
+
+    def test_crud(self, etesync):
+        # Empty collections
+        assert len(list(etesync.list())) == 0
+
+        # Create
+        a = api.Calendar.create(etesync, get_random_uid(self), {'displayName': 'Test'})
+        b = api.AddressBook.create(etesync, get_random_uid(self), {'displayName': 'Test 2'})
+        assert a is not None
+        assert b is not None
+        # Still empty because we haven't saved
+        assert len(list(etesync.list())) == 0
+
+        # Fetch before saved:
+        with pytest.raises(exceptions.DoesNotExist):
+            etesync.get(a.journal.uid)
+
+        a.save()
+        assert 'Test' == list(etesync.list())[0].collection.display_name
+        assert len(list(etesync.list())) == 1
+        b.save()
+        assert len(list(etesync.list())) == 2
+
+        # Get
+        assert a.journal.uid == etesync.get(a.journal.uid).uid
+        assert b.journal.uid == etesync.get(b.journal.uid).uid
+
+        # Delete
+        a.delete()
+        assert len(list(etesync.list())) == 1
+        b.delete()
+        assert len(list(etesync.list())) == 0
+
+    def test_content_crud(self, etesync):
+        a = api.Calendar.create(etesync, get_random_uid(self), {'displayName': 'Test'})
+        b = api.AddressBook.create(etesync, get_random_uid(self), {'displayName': 'Test 2'})
+        c = api.Calendar.create(etesync, get_random_uid(self), {'displayName': 'Test 3'})
+
+        ev = api.Event.create(a, '2cd64f22-1111-44f5-bc45-53440af38cec',
+                              'BEGIN:VCALENDAR\r\nVERSION:2.0\r\nPRODID:+//Yo\r\nBEGIN:VEVENT\r\nDTSTAMP:20170324T164' +
+                              '747Z\r\nUID:2cd64f22-1111-44f5-bc45-53440af38cec\r\nDTSTART;VALUE\u003dDATE:20170324' +
+                              '\r\nDTEND;VALUE\u003dDATE:20170325\r\nSUMMARY:Feed cat\r\nSTATUS:CONFIRMED\r\nTRANSP:' +
+                              'TRANSPARENT\r\nEND:VEVENT\r\nEND:VCALENDAR\r\n')
+
+        # Try saving before the journal is saved
+        # FIXME: Test for the correct exception once we have it
+        with pytest.raises(Exception):
+            ev.save()
+
+        # Create the event
+        a.save()
+        ev.save()
+
+        assert ev.uid == a.get('2cd64f22-1111-44f5-bc45-53440af38cec').uid
+
+        # Trying to add an Event into an AddressBook
+        b.save()
+
+        # Wrong child in collection
+        # FIXME: Test for the correct exception once we have it
+        with pytest.raises(Exception):
+            api.Event.create(b, '2cd64f22-2222-44f5-bc45-53440af38cec', (
+                  'BEGIN:VCALENDAR\r\nVERSION:2.0\r\nPRODID:+//Yo\r\nBEGIN:VEVENT\r\nDTSTAMP:20170324T164' +
+                  '747Z\r\nUID:2cd64f22-b51c-44f5-bc45-53440af38cec\r\nDTSTART;VALUE\u003dDATE:20170324' +
+                  '\r\nDTEND;VALUE\u003dDATE:20170325\r\nSUMMARY:Feed cat\r\nSTATUS:CONFIRMED\r\nTRANSP:' +
+                  'TRANSPARENT\r\nEND:VEVENT\r\nEND:VCALENDAR\r\n'))
+
+        # Same uid in different collections
+        c.save()
+        ev2 = api.Event.create(c, ev.uid, ev.content)
+        ev2.save()
+
+        # # First is still here even after we delete the new one
+        ev2.delete()
+        assert ev.uid == a.get('2cd64f22-1111-44f5-bc45-53440af38cec').uid
