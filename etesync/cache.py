22fccf6d9e8ad5d1f51ebb891561bf26ee1b937e
Tom Hacohen <tom@stosb.com>
2017-04-04 22:07:12 +0100

    Cache: change journal and entry content to be strings (since they are).

--- etesync/cache.py
+++ etesync/cache.py
@@ -11,7 +11,7 @@ class JournalEntity(db.BaseModel):
     owner = pw.ForeignKeyField(User, related_name='journals')
     version = pw.IntegerField()
     uid = pw.CharField(unique=True, null=False, index=True)
-    content = pw.BlobField()
+    content = pw.TextField()
     new = pw.BooleanField(null=False, default=False)
     dirty = pw.BooleanField(null=False, default=False)
     deleted = pw.BooleanField(null=False, default=False)
@@ -20,7 +20,7 @@ class JournalEntity(db.BaseModel):
 class EntryEntity(db.BaseModel):
     journal = pw.ForeignKeyField(JournalEntity, related_name='entries')
     uid = pw.CharField(unique=True, null=False, index=True)
-    content = pw.BlobField()
+    content = pw.TextField()
     new = pw.BooleanField(null=False, default=False)
 
     class Meta:
