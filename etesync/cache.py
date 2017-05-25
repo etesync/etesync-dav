5ac2714603988e1e60221be81809969ee4fc1b5c
Tom Hacohen <tom@stosb.com>
2017-05-25 16:22:21 +0100

    Cache: change unique constraints on uid reuse to be per user/journal.
    This is needed for shared calendar support, since for that we could
    potentially have the same journal id twice in the local database from
    two different users.

--- etesync/cache.py
+++ etesync/cache.py
@@ -10,18 +10,26 @@ class User(db.BaseModel):
 class JournalEntity(db.BaseModel):
     local_user = pw.ForeignKeyField(User, related_name='journals')
     version = pw.IntegerField()
-    uid = pw.CharField(unique=True, null=False, index=True)
+    uid = pw.CharField(null=False, index=True)
     content = pw.TextField()
     new = pw.BooleanField(null=False, default=False)
     dirty = pw.BooleanField(null=False, default=False)
     deleted = pw.BooleanField(null=False, default=False)
 
+    class Meta:
+        indexes = (
+            (('local_user', 'uid'), True),
+        )
+
 
 class EntryEntity(db.BaseModel):
     journal = pw.ForeignKeyField(JournalEntity, related_name='entries')
-    uid = pw.CharField(unique=True, null=False, index=True)
+    uid = pw.CharField(null=False, index=True)
     content = pw.TextField()
     new = pw.BooleanField(null=False, default=False)
 
     class Meta:
+        indexes = (
+            (('journal', 'uid'), True),
+        )
         order_by = ('id', )
