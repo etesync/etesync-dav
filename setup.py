fcd66ffa295e59c9c882e6a71f7d1841a8563fbc
Tom Hacohen <tom@stosb.com>
2017-05-30 20:38:39 +0100

    Bump version.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='etesync',
-    version='0.3.2',
+    version='0.4.0',
     author='EteSync',
     author_email='development@etesync.com',
     url='https://github.com/etesync/pyetesync',
@@ -16,14 +16,22 @@ setup(
     include_package_data=True,
     install_requires=[
        'appdirs>=1.4',
+       'asn1crypto>=0.22',
+       'cffi>=1.10',
+       'coverage>=4.3',
+       'cryptography>=1.9',
        'furl>=0.5',
+       'idna>=2.5',
        'orderedmultidict>=0.7',
        'packaging>=16.8',
        'peewee>=2.9',
-       'pyaes>=1.6',
+       'py>=1.4',
+       'pyasn1>=0.2',
+       'pycparser>=2.17',
        'pyparsing>=2.2',
        'pyscrypt>=1.6',
        'python-dateutil>=2.6',
+       'requests>=2.13',
        'six>=1.10',
        'vobject>=0.9',
     ]
