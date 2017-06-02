4214c87c93d68fbf2a66535d1a251aa6838ac049
Tom Hacohen <tom@stosb.com>
2017-06-02 17:20:02 +0100

    Setup.py: update and add proper description for pypi.

---
+++ DESCRIPTION.rst
@@ -0,0 +1,7 @@
+This is an `EteSync <https://www.etesync.com>`_ storage plugin for `Radicale <http://radicale.org/>`_.
+
+This plugin makes Radicale use EteSync as the storage module, essentially
+making Radicale a CalDav/CardDav frontend for EteSync.
+
+| Source code: https://github.com/etesync/radicale_storage_etesync
+| README: https://github.com/etesync/radicale_storage_etesync/blob/master/README.md
--- setup.py
+++ setup.py
@@ -1,5 +1,21 @@
-#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 
-from distutils.core import setup
+from setuptools import find_packages, setup
 
-setup(packages=["radicale_storage_etesync"])
+setup(
+    name='radicale_storage_etesync',
+    version='0.1.0',
+    author='EteSync',
+    author_email='development@etesync.com',
+    url='https://github.com/etesync/radicale_storage_etesync',
+    description='An EteSync storage plugin for Radicale',
+    keywords=['etesync', 'encryption', 'sync', 'pim', 'radicale'],
+    license='GPL',
+    long_description=open('DESCRIPTION.rst').read(),
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'etesync>=0.5.0',
+        'Radicale>=2.0.0',
+    ]
+)
