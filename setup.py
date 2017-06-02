e582c635434322fa966fa5217f092f5e519c7f60
Tom Hacohen <tom@stosb.com>
2017-06-02 17:27:19 +0100

    Setup.py: add missing manifest file.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='radicale_storage_etesync',
-    version='0.1.0',
+    version='0.1.1',
     author='EteSync',
     author_email='development@etesync.com',
     url='https://github.com/etesync/radicale_storage_etesync',
