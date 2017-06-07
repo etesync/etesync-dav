9597dd4688ced7c3ec138da6f5c6b2716216c547
Tom Hacohen <tom@stosb.com>
2017-06-07 16:10:35 +0100

    Bump version.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='radicale_storage_etesync',
-    version='0.1.1',
+    version='0.1.2',
     author='EteSync',
     author_email='development@etesync.com',
     url='https://github.com/etesync/radicale_storage_etesync',
