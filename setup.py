6c3360e309a1f8bd4233e6dfaf1902bc82f8ed2d
Tom Hacohen <tom@stosb.com>
2017-06-29 12:07:22 +0100

    Bump version.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='etesync-dav',
-    version='0.1.0',
+    version='0.1.1',
     author='EteSync',
     author_email='development@etesync.com',
     url='https://github.com/etesync/etesync-dav',
