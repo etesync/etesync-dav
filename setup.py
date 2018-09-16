3a7414b7dcdf030e986e5e878e6775da2231d995
Tom Hacohen <tom@stosb.com>
2018-09-16 20:32:54 +0100

    Bump pyetesync dep.

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
-        'etesync>=0.5.4',
+        'etesync>=0.5.5',
         'Radicale>=2.1.10',
     ]
 )
