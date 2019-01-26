f2f8041a51cff33e9f014ab2a6f325985ca82076
Tom Hacohen <tom@stosb.com>
2019-01-26 19:37:18 +0000

    Docker: pass all parameters to entry point and change invocation.
    This makes it possible to run etesync-dav-manage and etesync-dav with
    custom parameters which is great for debugging and greater flexibility.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-if [ "$1" == "setup" ]; then
-  read -p "Please enter the EteSync username: " USERNAME
-  etesync-dav-manage add ${USERNAME}
+if [ "$1" == "manage" ]; then
+  shift 1
+  etesync-dav-manage "$@"
   exit 0
 fi
 
@@ -10,4 +10,4 @@ echo "Upgrading etesync-dav if necessary"
 pip install --upgrade etesync-dav scrypt
 
 echo "Running etesync-dav"
-exec etesync-dav
+exec etesync-dav "$@"
