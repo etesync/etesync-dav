2bafa479a70b52407b98bfd1dcb01eb5e9f85b2a
Tom Hacohen <tom@stosb.com>
2019-02-07 14:55:02 +0000

    Bump versions of deps.

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
-        'etesync>=0.6.1',
+        'etesync>=0.7.0',
         'Radicale>=2.1.10',
     ]
 )
