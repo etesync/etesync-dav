9d3a3c706d188b6995420e7575160faad1989ec5
Tom Hacohen <tom@stosb.com>
2019-07-03 13:35:05 +0100

    Bump version.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='etesync',
-    version='0.8.2',
+    version='0.8.3',
     author='EteSync',
     author_email='development@etesync.com',
     url='https://github.com/etesync/pyetesync',
