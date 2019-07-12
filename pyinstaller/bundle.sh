143eb3c2f5c58bba75df6dab9ca303ecace8b48d
Tom Hacohen <tom@stosb.com>
2019-07-12 09:06:49 +0100

    Pyinstaller: removed radicale hook (merged upstream).

--- pyinstaller/bundle.sh
+++ pyinstaller/bundle.sh
@@ -3,6 +3,5 @@
 pyinstaller \
     --hidden-import radicale_storage_etesync \
     --hidden-import radicale_storage_etesync.rights \
-    --additional-hooks-dir ./hooks \
     --onefile \
     ../scripts/etesync-dav
--- pyinstaller/hooks/hook-radicale.py
+++
@@ -1,4 +0,0 @@
-from PyInstaller.utils.hooks import copy_metadata, collect_data_files
-
-datas = copy_metadata('radicale')
-datas += collect_data_files('radicale')
