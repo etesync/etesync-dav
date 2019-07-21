4fcd09587ebaa0df1459885974ac552170a7cf38
Tom Hacohen <tom@stosb.com>
2019-07-21 08:42:57 +0100

    Config: support overriding the database filename from env.
    This is needed now that we don't load it from the configuration anymore.

--- etesync_dav/config.py
+++ etesync_dav/config.py
@@ -4,9 +4,10 @@ import os
 LISTEN_ADDRESS = os.environ.get('ETESYNC_LISTEN_ADDRESS', 'localhost')
 LISTEN_PORT = os.environ.get('ETESYNC_LISTEN_PORT', '37358')
 CONFIG_DIR = os.environ.get('ETESYNC_CONFIG_DIR', user_config_dir("etesync-dav", "etesync"))
-HTPASSWD_FILE = os.path.join(CONFIG_DIR, 'htpaswd')
-CREDS_FILE = os.path.join(CONFIG_DIR, 'etesync_creds')
 ETESYNC_URL = os.environ.get('ETESYNC_URL', 'https://api.etesync.com/')
-DATABASE_FILE = os.path.join(CONFIG_DIR, 'etesync_data.db')
+DATABASE_FILE = os.environ.get('ETESYNC_DATABASE_FILE', os.path.join(CONFIG_DIR, 'etesync_data.db'))
 
 ETESYNC_MANAGEMENT_URL = os.environ.get('ETESYNC_MANAGEMENT_URL', 'http://localhost:37359')
+
+HTPASSWD_FILE = os.path.join(CONFIG_DIR, 'htpaswd')
+CREDS_FILE = os.path.join(CONFIG_DIR, 'etesync_creds')
