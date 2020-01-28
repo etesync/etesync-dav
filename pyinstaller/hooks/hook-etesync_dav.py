7954988f92974314f0bfc17c332353e1034db612
Tom Hacohen <tom@stosb.com>
2020-01-28 12:30:32 +0200

    PyInstaller: workaround a setuptools issue causing binaries to not run.
    Reference: https://github.com/pypa/setuptools/issues/1963

--- pyinstaller/hooks/hook-etesync_dav.py
+++ pyinstaller/hooks/hook-etesync_dav.py
@@ -1,4 +1,6 @@
-from PyInstaller.utils.hooks import copy_metadata, collect_data_files
+from PyInstaller.utils.hooks import copy_metadata, collect_data_files, collect_submodules
 
 datas = copy_metadata('etesync_dav')
 datas += collect_data_files('etesync_dav')
+
+hiddenimports = collect_submodules('pkg_resources')
