a55650aab521044ea354070e184e8fb93ec03384
Tom Hacohen <tom@stosb.com>
2020-02-13 10:16:37 +0200

    Bump version.

--- etesync_dav/_version.py
+++ etesync_dav/_version.py
@@ -12,4 +12,4 @@
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 
-__version__ = "0.14.2"
+__version__ = "0.14.3"
