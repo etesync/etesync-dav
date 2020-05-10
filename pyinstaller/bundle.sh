be3a1ec2df5305891a3979ef00e5ae41b3052fda
Tom Hacohen <tom@stosb.com>
2020-05-10 12:23:00 +0100

    pyinstaller: add mac icon.

--- pyinstaller/bundle.sh
+++ pyinstaller/bundle.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
+# Icon is used on Mac and ignored on Linux
 pyinstaller \
     --hidden-import etesync_dav.radicale \
     --additional-hooks-dir ./hooks \
     --onefile \
     --windowed \
+    --icon ./ic_launcher.icns \
     ../scripts/etesync-dav
