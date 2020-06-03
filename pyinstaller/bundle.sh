8bf5a4138b457637a3e7e9783e67292f2e2df4fd
Tom Hacohen <tom@stosb.com>
2020-06-03 12:05:06 +0300

    Pyinstaller: get it to build again.

--- pyinstaller/bundle.sh
+++ pyinstaller/bundle.sh
@@ -3,6 +3,7 @@
 # Icon is used on Mac and ignored on Linux
 pyinstaller \
     --hidden-import etesync_dav.radicale \
+    --hidden-import radicale.auth.htpasswd \
     --additional-hooks-dir ./hooks \
     --onefile \
     --windowed \
