75163661e685e64a9c6901b1ad7bbca1381e6271
Tom Hacohen <tom@stosb.com>
2020-08-09 10:47:38 +0300

    Add travis for CI/CD.
    This will make deployment easier, especially for platforms we don't
    have.

--- pyinstaller/bundle.sh
+++ pyinstaller/bundle.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
+set -e
+
+ICON=ic_launcher.icns
+if [ "$TRAVIS_OS_NAME" = "windows" ]; then
+    ICON=ic_launcher.ico
+fi
+
 # Icon is used on Mac and ignored on Linux
 pyinstaller \
     --hidden-import etesync_dav.radicale \
@@ -7,5 +14,20 @@ pyinstaller \
     --additional-hooks-dir ./hooks \
     --onefile \
     --windowed \
-    --icon ./ic_launcher.icns \
+    --icon $ICON \
     ../scripts/etesync-dav
+
+# Travis stuff
+mkdir deploy
+if [ "$TRAVIS_OS_NAME" = "linux" ]; then
+    mv dist/etesync-dav deploy/linux-etesync-dav
+fi
+if [ "$TRAVIS_OS_NAME" = "osx" ]; then
+    cd dist
+    zip -r mac-etesync-dav.zip *
+    mv mac-etesync-dav.zip ../deploy/
+    cd ../
+fi
+if [ "$TRAVIS_OS_NAME" = "windows" ]; then
+    mv dist/etesync-dav.exe deploy/
+fi
