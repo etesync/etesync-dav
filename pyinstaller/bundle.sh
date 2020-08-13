49472a36f536aca64a429d26e3b562b5b85a30f1
Tom Hacohen <tom@stosb.com>
2020-08-13 09:39:23 +0300

    Travis: test-run the binaries on linux and mac to see they compiled correctly.
    We can't do it on windows as the windows app doesn't accept CLI flags.

--- pyinstaller/bundle.sh
+++ pyinstaller/bundle.sh
@@ -20,9 +20,11 @@ pyinstaller \
 # Travis stuff
 mkdir deploy
 if [ "$TRAVIS_OS_NAME" = "linux" ]; then
+    ./dist/etesync-dav --version  # Sanity test on Linux and mac, can't do on windows
     mv dist/etesync-dav deploy/linux-etesync-dav
 fi
 if [ "$TRAVIS_OS_NAME" = "osx" ]; then
+    ./dist/etesync-dav --version  # Sanity test on Linux and mac, can't do on windows
     cd dist
     zip -r mac-etesync-dav.zip *
     mv mac-etesync-dav.zip ../deploy/
