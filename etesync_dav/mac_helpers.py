etesync/etesync-dav

commit 2769092f1e069772f77917ed65172edbcbc7491f

Update comment regarding cert creation constraints.
Following a comment in #181.

etesync_dav/mac_helpers.py
@@ -69,7 +69,7 @@ def generate_cert(cert_path: str = SSL_CERT_FILE, key_path: str = SSL_KEY_FILE,
 
     san = x509.SubjectAlternativeName(alt_names)
 
-    # path_len=0 means this cert can only sign itself, not other certs.
+    # prevent this cert form being used to sign other certs
     basic_contraints = x509.BasicConstraints(ca=False, path_length=0)
     now = datetime.utcnow()
     cert = (

commit 1bf96f201a84c4387adc81b1cbd26b660c56c4da

certgen: Fix SSL certificate generation
Fixes in issue introduced in 2769092f1e069772f77917ed65172edbcbc7491f

Fixes #186

etesync_dav/mac_helpers.py
@@ -70,7 +70,7 @@ def generate_cert(cert_path: str = SSL_CERT_FILE, key_path: str = SSL_KEY_FILE,
     san = x509.SubjectAlternativeName(alt_names)
 
     # prevent this cert form being used to sign other certs
-    basic_contraints = x509.BasicConstraints(ca=False, path_length=0)
+    basic_contraints = x509.BasicConstraints(ca=False, path_length=None)
     now = datetime.utcnow()
     cert = (
         x509.CertificateBuilder()