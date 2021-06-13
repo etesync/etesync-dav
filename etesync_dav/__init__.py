c4182f332bbcdcaa3a454c1afce69fa3fb170c65
Serghei Iakovlev <sergeyklay@users.noreply.github.com>
2021-06-13 17:18:09 +0300

    Improve setup module for etesync-dav (#222)

--- etesync_dav/__init__.py
+++ etesync_dav/__init__.py
@@ -1,4 +1,4 @@
-# Copyright © 2017 Tom Hacohen
+# Copyright © 2017-2021 Tom Hacohen <tom@stosb.com>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -12,4 +12,30 @@
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 
-from ._version import __version__  # noqa: F401
+"""The top-level module for etesync-dav.
+
+This module tracks the version of the package as well as the base
+package info used by various functions within etesync-dav.
+
+Misc variables:
+
+    __copyright__
+    __version__
+    __license__
+    __author__
+    __author_email__
+    __url__
+    __description__
+
+Refer to the `documentation <https://github.com/etesync/etesync-dav/>`_
+for details on the use of this package.
+
+"""
+
+__copyright__ = 'Copyright (C) 2017-2021 Tom Hacohen'
+__version__ = '0.30.8'
+__license__ = 'GPL-3.0-only'
+__author__ = 'Tom Hacohen'
+__author_email__ = 'tom@stosb.com'
+__url__ = 'https://github.com/etesync/etesync-dav'
+__description__ = 'A CalDAV and CardDAV frontend for EteSync'
