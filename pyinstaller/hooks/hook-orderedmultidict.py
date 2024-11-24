70679411c2941b057d0770b4fb419fc1f7a8dbbc
Tom Hacohen <tom@stosb.com>
2024-11-24 13:48:28 -0500

    Add a pyinstall hook to workaround orderedmultidict issue.
    https://github.com/gruns/orderedmultidict/issues/25 and
    https://github.com/gruns/orderedmultidict/issues/29

---
+++ pyinstaller/hooks/hook-orderedmultidict.py
@@ -0,0 +1,5 @@
+from PyInstaller.utils.hooks import collect_data_files
+
+module_collection_mode = "py+pyz"
+hiddenimports = ["orderedmultidict.__version__"]
+datas = collect_data_files('orderedmultidict')
