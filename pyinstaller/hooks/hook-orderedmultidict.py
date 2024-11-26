6fa4f21edf563bc3d6f58d7e17e6615498317261
Tom Hacohen <tom@stosb.com>
2024-11-26 15:41:26 -0500

    Reformat pyinstall hook.

--- pyinstaller/hooks/hook-orderedmultidict.py
+++ pyinstaller/hooks/hook-orderedmultidict.py
@@ -2,4 +2,4 @@ from PyInstaller.utils.hooks import collect_data_files
 
 module_collection_mode = "py+pyz"
 hiddenimports = ["orderedmultidict.__version__"]
-datas = collect_data_files('orderedmultidict')
+datas = collect_data_files("orderedmultidict")
