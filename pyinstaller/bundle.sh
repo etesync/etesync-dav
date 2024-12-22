1c71bfc9a9b4802032be7b60590da1b16633351a
Tom Hacohen <tom@stosb.com>
2024-12-22 09:14:55 -0500

    Pyinstaller: add missing radicale hook.
    Fixes #331 and #334.

--- pyinstaller/bundle.sh
+++ pyinstaller/bundle.sh
@@ -11,6 +11,7 @@ fi
 pyinstaller \
     --hidden-import etesync_dav.radicale \
     --hidden-import radicale.auth.htpasswd \
+    --hidden-import radicale.hook.none \
     --additional-hooks-dir ./hooks \
     --onefile \
     --windowed \
